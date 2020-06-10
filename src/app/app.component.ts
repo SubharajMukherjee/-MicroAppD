ead02070783f225c3ddba0ae79064d186044e63c
Subharaj Mukherjee <subharaj.mukherjee@gmail.com>
2020-06-10 13:11:16 +0530

    create repo

---
+++ src/app/app.component.ts
@@ -0,0 +1,20 @@
+import { Component } from '@angular/core';
+ 
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.css']
+})
+export class AppComponent {
+  title = 'elements1';
+
+  Button1() {
+    localStorage.setItem('demo', 'button1');
+    location.reload();
+  }
+
+  Button2() {
+    localStorage.setItem('demo', 'button2');
+    location.reload();
+  }
+}
---
+++ src/app/app.module.ts
@@ -0,0 +1,43 @@
+// import { BrowserModule } from '@angular/platform-browser';
+// import { NgModule } from '@angular/core';
+
+// import { AppComponent } from './app.component';
+
+// @NgModule({
+//   declarations: [
+//     AppComponent
+//   ],
+//   imports: [
+//     BrowserModule
+//   ],
+//   providers: [],
+//   bootstrap: [AppComponent]
+// })
+// export class AppModule { }
+
+/////////////////////////////////
+
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule, Injector } from '@angular/core';
+import { createCustomElement } from '@angular/elements';
+
+import { AppComponent } from './app.component';
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule
+  ],
+  providers: [],
+  entryComponents: [AppComponent]
+})
+
+export class AppModule {
+  constructor(private injector: Injector) {
+    const myElement = createCustomElement(AppComponent, { injector });
+    customElements.define('app-element1', myElement);
+  }
+  ngDoBootstrap() {}
+}
